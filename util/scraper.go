0c140a5337dfe9533b531f23a21de26dd086bd6d
nick <nicholasjdaniel@gmail.com>
2019-01-16 22:14:04 -0500

    Scrape with colly

--- util/scraper.go
+++ util/scraper.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"github.com/gocolly/colly"
 	"io"
 	"io/ioutil"
 	"log"
@@ -10,12 +11,55 @@ import (
 	"strings"
 )
 
+type Item struct {
+	Name         string
+	Price        string
+	Damage       string
+	Bulk         string
+	Hands        string
+	Group        string
+	WeaponTraits string
+}
+
 func main() {
-	PullTable()
+	// Instantiate default collector
+	c := colly.NewCollector()
+	fmt.Println("Scraping")
+
+	items := make([]Item, 0, 500)
+
+	// Find table
+	c.OnHTML("tbody", func(e *colly.HTMLElement) {
+		fmt.Println("found tbody?")
+		table := e.Attr("tbody")
+		fmt.Println(table)
+		item := Item{}
+		e.ForEach("tr", func(_ int, element *colly.HTMLElement) {
+
+			switch element.ChildText("td:first-child") {
+			case "Name":
+				item.Name = element.ChildText("td:nth-child(2)")
+			case "Price":
+				item.Price = element.ChildText("td:nth-child(2)")
+			case "Damage":
+				item.Damage = element.ChildText("td:nth-child(2)")
+			case "Bulk":
+				item.Bulk = element.ChildText("td:nth-child(2)")
+
+			}
+		})
+		items = append(items, item)
+	})
+
+	// Start scraping http://pf2playtest.opengamingnetwork.com/equipment/weapons/
+	c.Visit("http://pf2playtest.opengamingnetwork.com/equipment/weapons/")
+
+	fmt.Println(items)
+
 }
 
 //PullTable grab data from table in html and create json file
-func PullTable() {
+func MyPullTable() {
 	//open output.html
 	dataInBytes, err := ioutil.ReadFile("output.html")
 	if err != nil {
