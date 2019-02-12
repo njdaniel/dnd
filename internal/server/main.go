b9c83e769a2e63a21d8861e2b8c46d8adbccffa9
nick <nicholasjdaniel@gmail.com>
2019-02-11 20:34:47 -0500

    Add grpc server

--- internal/server/main.go
+++ internal/server/main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"log"
+	"net"
+
 	"github.com/njdaniel/dnd/internal/list"
-	"google.golang.org/grpc"
+	grpc "google.golang.org/grpc"
 )
 
 // run grpc server that interacts with fileserver
@@ -11,6 +14,11 @@ func main() {
 	//fmt.Println("in main")
 	srv := grpc.NewServer()
 	list.RegisterFilesServer(srv, files)
+	l, err := net.Listen("tcp", ":8888")
+	if err != nil {
+		log.Fatalf("could not listen to port :8888: %v", err)
+	}
+	log.Fatal(srv.Serve(l))
 }
 
 type fileServer struct{}
