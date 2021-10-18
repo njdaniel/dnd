b5469c1f27c9d825045850b0ca9576bf32c31b93
njdaniel <nicholasjdaniel@gmail.com>
2021-10-18 16:19:05 -0400

    data structures for inventory

--- services/store/store.go
+++ services/store/store.go
@@ -5,15 +5,44 @@ type StoreType int
 const (
 	Tavern StoreType = iota + 1
 	Blacksmith
-	Alchemy
+	Apothecary
 	Clothing
 	GeneralGoods
 	Bowyer
 )
+
+////////Item Quality ////////////
+
+type Quality int
+
+const (
+	Poor Quality = iota + 1
+	Average
+	Good
+	Great
+)
+
+///////////////////////////////
+
+type Item struct {
+	Name        string  `json:"name"`
+	Description string  `json:"description"`
+	Quality     Quality `json:"quality"`
+}
+
+type Inventory struct {
+	Items []Item `json:"items"`
+}
 
 type Store struct {
 	Name      string    `json:"name"`
 	Owner     string    `json:"owner"`
 	StoreType StoreType `json:"store_type"`
 	Inventory Inventory `json:"inventory"`
+}
+
+func NewStore() Store {
+	ns := Store{}
+
+	return ns
 }
