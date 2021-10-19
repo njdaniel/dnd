e2156183444168ccaeddfea2ef17bf27d4f1613f
njdaniel <nicholasjdaniel@gmail.com>
2021-10-19 12:01:12 -0400

    add store data structure

--- services/store/store.go
+++ services/store/store.go
@@ -26,34 +26,42 @@ const (
 ///////////////////////////////
 
 // Item is the generic physical object
-type Item interface {
-	Name() string
-	Description() string
-	Price() string
-}
-
-// Inventory contains a list of items of multiude of different types
-type Inventory struct {
-	Items []Item `json:"items"`
-}
+//type Item interface {
+//	Name() string
+//	Description() string
+//	Price() string
+//}
 
 // Store represents the physical entity of a business
 type Store struct {
-	Name      string    `json:"name"`
-	Owner     string    `json:"owner"`
-	StoreType StoreType `json:"store_type"`
-	Inventory Inventory `json:"inventory"`
+	Name      string `json:"name"`
+	Owner     string `json:"owner"`
+	Location  string `json:"location"`
+	Inventory []Item `json:"inventory"`
+	Money     Money  `json:"money"`
+}
+
+// Item contains  of items of multiude of different types
+type Item struct {
+	Name   string `json:"name"`
+	Price  string `json:"price"`
+	Weight string `json:"weight"`
+}
+
+// Money has the types of money
+type Money struct {
+	BrassPennies    int `json:"Brass Pennies"`
+	SilverShillings int `json:"Silver Shillings"`
+	GoldCrowns      int `json:"Gold Crowns"`
 }
 
 // NewStore simple factory of creating the object Store
-func NewStore(name, owner string, items []Item) Store {
+func NewStore(name, owner string, items ...Item) Store {
 	ns := Store{}
 	ns.Name = name
 	ns.Owner = owner
 
-	inventory := Inventory{
-		Items: items,
-	}
+	inventory := items
 	ns.Inventory = inventory
 
 	return ns
