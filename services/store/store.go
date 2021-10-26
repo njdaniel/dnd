1b480a6d7f5c24a3fa482d591604bf408bb97d2e
njdaniel <nicholasjdaniel@gmail.com>
2021-10-26 01:07:24 -0400

    add arrow enums

--- services/store/store.go
+++ services/store/store.go
@@ -1,6 +1,7 @@
 package store
 
 import (
+	"errors"
 	"fmt"
 
 	"github.com/njdaniel/dnd/services/commands/character"
@@ -52,7 +53,7 @@ func VerifyEnum(s string, enum character.Enum) bool {
 ///////////////////////
 //Builder Pattern
 
-type iStoreBuilder interface {
+type IStoreBuilder interface {
 	setStoreType()
 	setName()
 	setOwner()
@@ -60,37 +61,90 @@ type iStoreBuilder interface {
 	setInventory()
 }
 
-func getStoreBuilder(storeBuilderType string) iStoreBuilder {
+func GetStoreBuilder(storeBuilderType string) IStoreBuilder {
 	fmt.Println(storeBuilderType)
-	isb := fletcherBuilder{}
+	isb := FletcherBuilder{}
 	return isb
 }
 
-type fletcherBuilder struct {
+type FletcherBuilder struct {
 	Name string
 	StoreType
 	Owner    string
 	Location string
 }
 
-func (f fletcherBuilder) setStoreType() {
+func (f FletcherBuilder) setStoreType() {
+	f.StoreType = 1
+}
+
+func (f FletcherBuilder) setName() {
+	f.Name = "Fletcher Shop"
+}
+
+func (f FletcherBuilder) setOwner() {
+	//TODO: call character.NewCharacter
+	f.Owner = "Bob"
+}
+
+func (f FletcherBuilder) setLocation() {
+	f.Location = "Heldheim"
+}
+
+func (f FletcherBuilder) setInventory() {
 
 }
 
-func (f fletcherBuilder) setName() {
+type Metal int
 
+const (
+	Brass Metal = iota + 1
+	Iron
+	Silver
+	Steel
+)
+
+func (m Metal) String() string {
+	return [...]string{"Brass", "Iron", "Silver", "Steel"}[m-1]
 }
 
-func (f fletcherBuilder) setOwner() {
-
+func (m Metal) Len() int {
+	return 4
 }
 
-func (f fletcherBuilder) setLocation() {
+type ArrowHeadType int
 
+const (
+	Pointed ArrowHeadTypes = iota + 1
+	Leaf
+	Broadhead
+	Barbed
+	Bodkin
+	Bludgeon
+)
+
+func (a ArrowHeadType) String() string {
+	return [...]string{"Pointed", "Leaf", "Broadhead", "Barbed", "Bodkin", "Bludgeon"}[a-1]
 }
 
-func (f fletcherBuilder) setInventory() {
+func (a ArrowHeadType) Len() int {
+	return 6
+}
 
+func (a ArrowHeadType) IsValid() error {
+	switch a {
+	case Pointed, Leaf, Broadhead, Barbed, Bodkin, Bludgeon:
+		return nil
+	}
+	return errors.New("Invalid ArrowHeadType")
+}
+
+type Arrow struct {
+	Weight
+	Price Money
+	Quality
+	HeadMaterial Metal
+	HeadType     ArrowHeadTypes
 }
 
 ////////////////////
